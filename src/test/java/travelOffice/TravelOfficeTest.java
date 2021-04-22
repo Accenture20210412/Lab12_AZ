d33ed7324a8d9e2fe410c18d9e1091d7cffbe0ba
Jakub Dutkiewicz <jak.dut@gmail.com>
2021-04-22 10:03:17 +0200

    Added second tests.

--- src/test/java/travelOffice/TravelOfficeTest.java
+++ src/test/java/travelOffice/TravelOfficeTest.java
@@ -3,17 +3,35 @@ package travelOffice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TravelOfficeTest {
+
+    private TravelOffice travelOffice = new TravelOffice();
 
     @Test
     public void shouldAddCustomer() {
         //given
         Customer customer = new Customer("Kuba", "Nowak", "Katowice");
-        TravelOffice travelOffice = new TravelOffice();
         //when
         travelOffice.addCustomer(customer);
         //then
         Assertions.assertTrue(travelOffice.getCustomersInfo().contains("Kuba"));
     }
+
+    @Test
+    public void shouldDeleteCustomersByName() {
+        //given
+        List<Customer> customers = new ArrayList<>();
+        Customer customer = new Customer("Kuba", "Nowak", "Katowice");
+        customers.add(customer);
+        //when
+        travelOffice.deleteCustomersByName("Kuba");
+        //then
+        Assertions.assertFalse(customers.contains(customer));
+    }
+
+
 
 }
